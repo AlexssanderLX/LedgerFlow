d47d935d1c1e78d8194cc7239ece82bb7c6dbb94
Alexssander Ferreira de Almeida <alexssander.f.almeida2006@email.com>
2026-01-15 07:05:02 -0300

    chore: initial project setup

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LedgerFlow.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
